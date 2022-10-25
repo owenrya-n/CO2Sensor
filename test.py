6c160fc67a6d15268c94df2ddc8e7730fba7129e
Metastable1883 <owenryan1883@gmail.com>
2022-10-25 13:05:48 -0700

    pppppp

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as map
-import logger
+import Logger
 map.plot(logger.RCVL[:,0],logger.RCVL[:,1])
 map.plot(logger.RCVL[:,0],logger.RCVL[:,2])
 map.style.use('_mpl-gallery')
