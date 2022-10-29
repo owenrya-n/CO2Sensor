57fb81716bc9e040e34701fe0282ac97cfc0681d
Metastable1883 <owenryan1883@gmail.com>
2022-10-29 13:49:59 -0700

    push

--- Logger.py
+++ Logger.py
@@ -7,7 +7,7 @@ import varis
 import pandas as pd
 
 #connect to Sourcemeter
-def Startup(port): #use arrays
+def Startup(port): 
     global srcm
     srcm = [None] * len(port)
     for k in range(0,len(port)):
@@ -42,25 +42,25 @@ def measureCVL(idet,timestep,pts,voltage):
         sleep(timestep)   
     global RCVL
     RCVL=np.empty((pts,3*len(idet)+1), dtype=np.float)
-    RCVL[:,5]=varis.svr #need to clean this up
-    RCVL[:,2]=varis.svr
-    RCVL[:,4]=ICVL[:,1]
-    RCVL[:,1]=ICVL[:,0]
-    RCVL[:,6]=np.true_divide(varis.svr,ICVL[:,1])
-    RCVL[:,3]=np.true_divide(varis.svr,ICVL[:,0])#np.true_divide(varis.svr,ICVL[:,:1]) # NEED LOOP OR COPY PASte
-    RCVL[:,:1]=times # DOES NOT YET SCALE WITH PORTS CONNECTED.
+    RCVL[:,:1]=times
+    for k in range(0,len(idet)):    #EXPERIMENTAL 
+        RCVL[:,3*k+2]=varis.svr     #TRUE CODE IS COMMENTED OUT
+        RCVL[:,3*k+1]=ICVL[:,k]     #NEED TO HANDLE BUGS
+        RCVL[:,3*k+3]=np.true_divide(varis.svr,ICVL[:,k])
+    #RCVL[:,4]=ICVL[:,1]
+    #RCVL[:,5]=varis.svr
+    #RCVL[:,6]=np.true_divide(varis.svr,ICVL[:,1])
 
 def save(arrayname,filename): 
     frame=pd.DataFrame(arrayname)
     if(len(arrayname[1,:]))==7:
         frame.columns=['time(s)','I1','V1','R1','I2','V2','R2']
     frame.to_csv(filename)
-    print('save successful')
+    print('Save Successful')
 
-def measureSIV(loc,minV,maxV,pts):
+def measureSIV(loc,minV,maxV,pts): #deprecated
     Vrange=np.arange(minV,maxV,(maxV-minV)/pts).reshape(pts,1)
     Ivals=np.zeros_like(Vrange)
-    print(Vrange)
     for i in range(pts):
         srcm[loc].measure_current()
         srcm[loc].ramp_to_voltage(Vrange[i,0],2,.001)
@@ -70,11 +70,33 @@ def measureSIV(loc,minV,maxV,pts):
         print(i)
     global IV
     IV=np.empty([pts,2])
-    #IV[:,1]=times
-    IV[:,0]=Vrange[:,0] #clean up this as well
+    IV[:,0]=Vrange[:,0] 
     IV[:,1]=Ivals[:,0]
+    print('IV Profile Generated')
 
-
+def measureRCTC(idet,resistance,Vin,pts,Htime,Ltime):
+    global HVoltages
+    global LVoltages
+    global Distimes
+    global Capacitances
+    HVoltages = np.zeros(len(idet),pts)
+    LVoltages = np.zeros(len(idet),pts)
+    Distimes = np.zeros(1,pts)+Ltime
+    Capacitances = np.zeros(len(idet),pts)
+    for k in range(0,len(idet)):
+        srcm[k].measure_voltage()
+    for j in range(0,pts): #this is bad I need to fix it
+        for l in range(0,len(idet)):
+            srcm[l].ramp_to_voltage(Vin,1,0)#can I set this to 0.0?
+            sleep(Htime) #set this to something appropriate
+            HVoltages[l,j]=srcm[l].voltage
+        for n in range(0,len(idet)): #definitely need to restructure this
+            srcm[n].ramp_to_voltage(0,1,0)#can I set this to 0.0?
+            sleep(Ltime) #set this to something appropriate
+            LVoltages[n,j]=srcm[n].voltage
+    Capacitances = np.reciprocal(np.log(np.divide(HVoltages,LVoltages)))*(-1)*Ltime/resistance
+    
+    
 def shutdown(port):
     for k in range(0,len(port)):
         srcm[k].shutdown()
